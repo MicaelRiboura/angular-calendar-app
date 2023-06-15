4870082994f8bc38ba683575fca6a96160ac8084
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-06-15 11:58:23 -0300

    feat(calendar): add structure to paginators

--- src/app/pages/calendar/calendar.module.ts
+++ src/app/pages/calendar/calendar.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarComponent } from './calendar.component';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 const routes = [ { path: '', component: CalendarComponent }];
 
 @NgModule({
   declarations: [
-    CalendarComponent
+    CalendarComponent,
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    MatButtonModule,
+    MatIconModule,
   ]
 })
 export class CalendarModule { }
