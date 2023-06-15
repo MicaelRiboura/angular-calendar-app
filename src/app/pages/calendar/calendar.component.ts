359db603a71bfdc704c1ceabb22150b79d11b547
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-06-15 10:43:03 -0300

    feat(calendar): add calendar service with dependency injection

--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CalendarService } from 'src/app/services/calendar.service';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit {
 
+  days: number[] = [];
+
+  constructor(private calendarService: CalendarService) { }
+
+  ngOnInit () {
+    this.days = this.calendarService.getDaysInMonth(2023, 6);
+  }
+
+  
 }
