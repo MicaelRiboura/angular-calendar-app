4d6a5fd24c498b9767912c903e7213bba1210719
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-06-15 15:10:53 -0300

    feat(create-appointment): add validation in form

--- src/app/pages/create-appointment/create-appointment.component.ts
+++ src/app/pages/create-appointment/create-appointment.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AppointmentService } from 'src/app/services/appointment.service';
 
 @Component({
@@ -19,14 +19,21 @@ export class CreateAppointmentComponent implements OnInit {
 
   ngOnInit() {
     this.form = this.formBuilder.group({
-      title: [null],
-      description: [null],
-      date: [null],
+      title: [null, Validators.required],
+      description: [null, Validators.required],
+      date: [null, Validators.required],
     });
   }
 
   onSubmit() {
-    console.log('submit: ', this.form.value);
-    this.appointmentService.saveAppointment(this.form);
+    if (this.form.valid) {
+      console.log('submit: ', this.form.value);
+      this.appointmentService.saveAppointment(this.form);
+      alert('salvo com sucesso');
+    }
+  }
+
+  validateField(field: string) {
+    return this.form.get(field)?.valid && this.form.get(field)?.touched;
   }
 }
