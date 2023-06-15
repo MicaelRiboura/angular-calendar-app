0059157d9154c6106f7d2e2441620b4071f2978a
MicaelRiboura <micaelribouradossantos@gmail.com>
2023-06-15 16:39:49 -0300

    feat: create appointment model

---
+++ src/app/models/appointment.model.ts
@@ -0,0 +1,24 @@
+import { Deserializable } from "./deserializable.interface";
+
+export class Appointment implements Deserializable<Appointment> {
+    title: string;
+    description: string;
+    date: Date;
+
+    constructor() {
+        this.title = '';
+        this.description = '';
+        this.date = new Date();
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    deserialize(input: any): Appointment {
+        if (input.date) {
+            this.date = new Date(input.date);
+            delete input.date;
+        }
+
+        Object.assign(this, input);
+        return this;
+    }
+}
